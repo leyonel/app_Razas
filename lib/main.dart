2ffa48f4ec810f4a035c41a0f3094c3f7d505a6f
leyonel <leonellayonel@gmail.com>
2022-10-02 20:15:43 -0600

    Implementacion de bloc en main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'caracteristicas/verificacion/bloc.dart';
+
 void main() {
   runApp(const MiAplicacionCanina());
 }
@@ -10,6 +12,32 @@ class MiAplicacionCanina extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return BlocProvider(
+      create: (context) {
+        BlocVerificacion blocVerificacion = BlocVerificacion();
+        Future.delayed(Duration(seconds: 1));
+        return blocVerificacion;
+      },
+      child: Aplicacion(),
+    );
+  }
+}
+
+class Aplicacion extends StatelessWidget {
+  const Aplicacion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Aplicacion',
+        home: Scaffold(body: Builder(
+          builder: ((context) {
+            var estado = context.watch<BlocVerificacion>().state;
+            return const Center(
+              child: Text("Regresa!"),
+            );
+          }),
+        )));
   }
 }
