998d1df58105d3767766431301cec688f9085b1f
leyonel <leonellayonel@gmail.com>
2022-10-05 20:38:25 -0600

    adding test json incorrect in repositorio_verifica

--- test/caracteristicas/repositorio_verificacion_test.dart
+++ test/caracteristicas/repositorio_verificacion_test.dart
@@ -1,4 +1,5 @@
 import 'package:app_doggys/caracteristicas/repositorio_verificacion.dart';
+import 'package:app_doggys/dominio/problemas.dart';
 import 'package:app_doggys/dominio/varibale_dominio.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -54,6 +55,19 @@ void main() {
         expect(
             r.mensaje, equals("Breed not found (master breed does not exist)"));
         expect(r.status, equals('error'));
+      });
+    });
+
+    test('Con incorrecto me regresa de json incorrecto', () {
+      RepositorioPruebasVerificacion repositorio =
+          RepositorioPruebasVerificacion();
+
+      var resultado = repositorio
+          .obtenerRegistroRaza(NickFormado.constructor('incorrecto'));
+      resultado.match((l) {
+        expect(l, isA<VersionIncorrectaJSON>());
+      }, (r) {
+        assert(false);
       });
     });
   });
